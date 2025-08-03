36e2e177ec2629c8108a08f39f5384080331d3e4
Th3Catalyst <max675295@gmail.com>
2025-08-03 21:45:24 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,12 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     let content = this.nextElementSibling;
+    console.log(content.style.display);
     if (content.style.display != "none") {
       content.style.display = "none";
     } else {
       content.style.display = "block";
     }
+    console.log(content.style.display);
   });
 }
