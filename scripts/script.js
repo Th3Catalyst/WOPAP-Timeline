c9bb4797c897bd38b62849cdb62139dc0add5279
Th3Catalyst <max675295@gmail.com>
2025-09-16 22:30:37 -0700

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -107,8 +107,10 @@ document.getElementById("searchTag").addEventListener("focus", (e) => {
   input = document.getElementById("searchTag")
   setTimeout(function(){ input.selectionStart = input.selectionEnd = 10000; }, 0);
 document.getElementById("tagList").style.opacity = "1";
+document.getElementById("tagList").style.pointerEvents = "auto";
 });
 
 document.getElementById("searchTag").addEventListener("blur", (e) => {
-  document.getElementById("tagList").style.opacity = "0";
+document.getElementById("tagList").style.opacity = "0";
+document.getElementById("tagList").style.pointerEvents = "none";
 });
